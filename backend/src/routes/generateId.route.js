fa9f43a0fe5f0ee2105b6c8fba95620a006fee66
Justin <justin@justinf.dev>
2023-02-13 19:12:58 -0800

    Stringify response JSON

--- backend/src/routes/generateId.route.js
+++ backend/src/routes/generateId.route.js
@@ -5,9 +5,9 @@ const controller = require('../controllers/id.controller');
 // Function to assign a player id to a player
 router.post('/', function(request, response) {
   console.log('POST /generate_id');
+  const body = { id: controller.current_player_id };
   response.writeHead(200, {'Content-Type': 'application/json'});
-  response.write({'id': controller.current_player_id});
-  response.end();
+  response.end(JSON.stringify(body));
   controller.current_player_id++; // Increment player pointer
 });
 
