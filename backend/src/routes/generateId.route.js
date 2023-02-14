1ad91b9d46940cc56b0e5b74974874aa8859d7e3
Justin <justin@justinf.dev>
2023-02-13 19:10:11 -0800

    Try to return json instead of plain

--- backend/src/routes/generateId.route.js
+++ backend/src/routes/generateId.route.js
@@ -5,8 +5,9 @@ const controller = require('../controllers/id.controller');
 // Function to assign a player id to a player
 router.post('/', function(request, response) {
   console.log('POST /generate_id');
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end(controller.current_player_id); // Return current player pointer
+  response.writeHead(200, {'Content-Type': 'application/json'});
+  response.write({'id': controller.current_player_id});
+  response.end();
   controller.current_player_id++; // Increment player pointer
 });
 
